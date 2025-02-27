f5f510adf9afa2b21c9e9b8354330f8637a2f22a
azzamreal <aworawor123456789@gmail.com>
2025-02-27 13:48:19 +0700

    Update autoLike.py
    Auto like

--- autoLike.py
+++ autoLike.py
@@ -2,10 +2,10 @@ import time
 import pyautogui
 import webbrowser
 
-webbrowser.open('https://www.facebook.com/nesarali.titumir')#put your friend's profile URL here
+webbrowser.open('https://www.facebook.com/azzamludin12')#put your friend's profile URL here
 
 no_of_posts = 10
-liked = 0
+liked = 100
 while(True):
     time.sleep(0.5)
 
